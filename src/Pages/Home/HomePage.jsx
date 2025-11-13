5463c3e4af71e2920017aeec1cd8a4884aa62ad6
mujahidraj <mujahidraj65@gmail.com>
2025-11-13 21:00:59 +0600

    project setup part 2

---
+++ src/Pages/Home/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      I am home Page
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { createBrowserRouter } from "react-router";
+import Root from '../Pages/Root/Root';
+import ErrorPage from '../Pages/Errors/ErrorPage';
+import HomePage from '../Pages/Home/HomePage';
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component : Root,
+    errorElement : <ErrorPage></ErrorPage>,
+    children : [
+      {
+        index:true,
+        path : "/",
+        Component: HomePage
+      }
+    ]
+  },
+]);
